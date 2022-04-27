3e1d4a5ecc6bc564a82f7630a2f5792571f82fa4
Cavin6080 <cavinmacwan1@gmail.com>
2022-04-27 22:42:59 +0530

    Youtube main screen done

--- lib/widgets/video_card.dart
+++ lib/widgets/video_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:youtube/data.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 class VideoCard extends StatelessWidget {
   final Video video;
@@ -36,7 +37,50 @@ class VideoCard extends StatelessWidget {
             ),
           ],
         ),
-        Row(),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              CircleAvatar(
+                foregroundImage: NetworkImage(video.author.profileImageUrl),
+              ),
+              const SizedBox(width: 12),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Flexible(
+                      child: Text(
+                        video.title,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyText1!
+                            .copyWith(fontSize: 15),
+                      ),
+                    ),
+                    Flexible(
+                      child: Text(
+                        '${video.author.username} | ${video.viewCount} | ${timeago.format(video.timestamp)}',
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyText1!
+                            .copyWith(fontSize: 14),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const Icon(Icons.more_vert),
+            ],
+          ),
+        ),
       ],
     );
   }
